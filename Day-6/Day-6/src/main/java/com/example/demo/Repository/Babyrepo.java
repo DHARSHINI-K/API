ac74953588114dd3fb573d22957aaf7c8b12c5df
DHARSHINI-K <113882668+DHARSHINI-K@users.noreply.github.com>
2023-02-28 16:20:07 +0530

    Delete Day-6/Day-6 directory

--- Day-6/Day-6/src/main/java/com/example/demo/Repository/Babyrepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.Entity.Babydetails;
-public interface Babyrepo extends JpaRepository<Babydetails,Integer> {
-
-}
