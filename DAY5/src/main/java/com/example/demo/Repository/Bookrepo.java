4c3480bb0d1c8cea4c546c84878e0694afb28903
DHARSHINI-K <113882668+DHARSHINI-K@users.noreply.github.com>
2023-02-28 16:19:23 +0530

    Delete DAY5 directory

--- DAY5/src/main/java/com/example/demo/Repository/Bookrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Book;
-
-public interface Bookrepo extends JpaRepository<Book,Integer>{
-
-}
